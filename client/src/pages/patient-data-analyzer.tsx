71701db2e5f43e970796a7a04842f5b76ee75bc7
Naveenravi07 <gamerkidnav@gmail.com>
2025-03-06 15:17:41 +0530

    Integrated Python server on client

--- client/src/pages/patient-data-analyzer.tsx
+++ client/src/pages/patient-data-analyzer.tsx
@@ -1,5 +1,6 @@
 
 import { useState } from "react"
+import Detection from "../components/detection"
 
 export default function PatientDataAnalyzer() {
   const [activeTab, setActiveTab] = useState<"detection" | "report-analyzer" | "finalize">("detection")
@@ -47,7 +48,7 @@ export default function PatientDataAnalyzer() {
 
         {/* Content Area - Empty for now */}
         <div className="mx-6 mb-6 min-h-[400px] border border-neutral-800 rounded-lg p-4">
-          {activeTab === "detection" && <div className="text-neutral-400">Detection tab content will go here</div>}
+          {activeTab === "detection" && <Detection/> }
           {activeTab === "report-analyzer" && (
             <div className="text-neutral-400">Report Analyzer tab content will go here</div>
           )}
