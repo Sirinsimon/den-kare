ea45b6a62d8314e7fe72728e6db03f92342b2c48
Manumanu1234 <manumanuvkm123@gmail.com>
2025-03-06 15:51:29 +0530

    rag

--- Python/object_detector.py
+++ Python/object_detector.py
@@ -58,5 +58,124 @@ def detect_objects_on_image(buf):
         output.append([x1, y1, x2, y2, result.names[class_id], prob_percentage])
     return output
 
+
+
+#rag---------------------------------------------------------------------------------------------------
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from langchain.agents import AgentType
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from langchain_experimental.tools.python.tool import PythonREPLTool
+from langchain.agents import initialize_agent, Tool,AgentType
+from llama_index.llms.openrouter import OpenRouter
+from llama_index.agent.openai import OpenAIAgent
+from llama_index.core.llms import ChatMessage
+from llama_index.experimental.query_engine import PandasQueryEngine
+from langchain_groq import ChatGroq
+import json
+import requests
+from bs4 import BeautifulSoup
+import ast
+import pandas as pd
+from yt_dlp import YoutubeDL
+from langchain_community.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.retrievers import EnsembleRetriever, BM25Retriever
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import DocumentCompressorPipeline, LLMChainFilter
+from langchain_community.document_transformers import EmbeddingsRedundantFilter
+from langchain.retrievers.document_compressors import FlashrankRerank
+from langchain.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+import re
+from pydantic import BaseModel, Field
+from typing import Optional
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from fastapi.middleware.cors import CORSMiddleware
+import google.generativeai as genai
+from datetime import datetime
+from langchain.document_loaders import TextLoader
+from langchain_cohere import CohereRerank
+api_key="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"
+groq = ChatGroq(
+    api_key=api_key,
+    model="gemma2-9b-it",
+)
+
+def load_and_process_data():
+    try:
+        #texts=text
+        loader = TextLoader("./manu.txt")
+        texts=loader.load()
+        chunking = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=30)
+        chunks = chunking.split_documents(texts)
+        db = FAISS.from_documents(chunks, GoogleGenerativeAIEmbeddings(google_api_key="AIzaSyCYft-LY8iU8aLtqvfYyOn4YRmsoa4Hl48", model="models/embedding-001"))
+        return db, chunks
+    except UnicodeDecodeError as e:
+        print(f"Error decoding file nothing: {e}")
+        raise
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        raise
+def Rag_Calling(final_retriver):
+    _redudentfilter = EmbeddingsRedundantFilter(embeddings=GoogleGenerativeAIEmbeddings(google_api_key="AIzaSyCYft-LY8iU8aLtqvfYyOn4YRmsoa4Hl48", model="models/embedding-001"))
+    rerank = CohereRerank(cohere_api_key="EA5kdJri7hsSOW2i801sXGQSZgW1iP5GwPsB3MF1",model="rerank-english-v3.0")
+    pipeline = DocumentCompressorPipeline(transformers=[_redudentfilter, rerank])
+    final_chain = ContextualCompressionRetriever(base_compressor=pipeline, base_retriever=final_retriver)
+    return final_chain
+
+
+class Medical_Old_report(BaseModel):
+    query:str
+
+@app.post("/medical_report")
+def Medical_Old_Reports(query:Medical_Old_report): 
+    db,chunks=load_and_process_data()
+    retriver1 = db.as_retriever(search_kwargs={"k": 4})
+    retriver2 = BM25Retriever.from_documents(chunks, k=4)
+    final_retriver = EnsembleRetriever(retrievers=[retriver1, retriver2], weights=[0.5, 0.5])
+    template = "You should answer the question based on the context. Context: {context} and Question: {question}"
+    prompt = PromptTemplate.from_template(template)
+    retriver = Rag_Calling(final_retriver)
+    chain = (
+            {
+                "context": retriver,
+                "question": RunnablePassthrough()
+            }
+            | prompt
+            | groq
+            | StrOutputParser()
+        )
+        
+    final_chain=chain
+    result=final_chain.invoke(query.query)
+    return {"result": result}
+
+
+#--------------------------------------------------------------Scan --------------------------------------------------------------------------------------
+
+import PIL.Image
+
+class Images(BaseModel):
+    image_link:str
+@app.post("/scan")
+def Scan(image:Images):
+    image_link=image.image_link
+    print(image_link)
+    image = PIL.Image.open(image_link)
+    client = genai.Client(api_key="AIzaSyCyB9rgwo4e0WoZS-CIulrcs_fzg2IEuwc")
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=["Analyze the provided dental X-ray/intraoral image and generate a detailed diagnosis report. Identify cavities, gum disease, misalignment, impacted teeth, bone loss, or any other abnormalities. Provide a structured report with findings, potential causes, and recommended treatments.", image])
+    return{"result":response.text}
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
